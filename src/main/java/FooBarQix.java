1c2028f0f483840ae9d429f4c2580e3642070865
vrojo <vin.rojo@gmail.com>
2019-04-29 09:51:21 +0200

    1st feature: Replace 3,5,7 by Foo, Bar, Qix Replace multiples of 3,5, 7 by Foo, Bar, Qix

---
+++ src/main/java/FooBarQix.java
@@ -0,0 +1,29 @@
+public class FooBarQix {
+
+    static String compute(int givenNbr) {
+        StringBuilder number = new StringBuilder();
+        if (givenNbr % 3 == 0) {
+            number.append("Foo");
+        }
+        if (givenNbr % 5 == 0) {
+            number.append("Bar");
+        }
+        if (givenNbr % 7 == 0) {
+            number.append("Qix");
+        }
+
+        String givenNumberString = Integer.toString(givenNbr);
+        for (String letter : givenNumberString.split("")) {
+            if (letter.equals("3"))
+                number.append("Foo");
+            if (letter.equals("5"))
+                number.append("Bar");
+            if (letter.equals("7"))
+                number.append("Qix");
+        }
+        if (number.toString().equals("")) {
+            return givenNumberString;
+        }
+        return number.toString();
+    }
+}
---
+++ src/test/java/FooBarQixTest.java
@@ -0,0 +1,110 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.assertj.core.api.Assertions;
+
+@RunWith(JUnitParamsRunner.class)
+public class FooBarQixTest {
+    @Test
+    @Parameters({
+            "1, 1",
+            "2, 2",
+            "4, 4",
+            "8, 8"
+    })
+    public void should_return_the_same_number_when_the_number_isnt_a_multiple_or_doesnt_contain_3_5_7(int originalNumber, String expectedString) {
+        String modifiedString = FooBarQix.compute(originalNumber);
+
+        Assertions.assertThat(modifiedString).isEqualTo(expectedString);
+    }
+
+    @Test
+    @Parameters({
+            "3, FooFoo",
+            "6, Foo",
+            "9, Foo",
+            "12, Foo",
+            "33, FooFooFoo"
+    })
+    public void should_return_Foo_when_the_number_is_a_multiple_of_3(int originalNumber, String expectedString) {
+        String modifiedString = FooBarQix.compute(originalNumber);
+
+        Assertions.assertThat(modifiedString).isEqualTo(expectedString);
+    }
+
+    @Test
+    @Parameters({
+            "5, BarBar",
+            "10, Bar",
+            "15, FooBarBar",
+            "100, Bar"
+    })
+    public void should_return_Foo_when_the_number_is_a_multiple_of_5(int originalNumber, String expectedString) {
+        String modifiedString = FooBarQix.compute(originalNumber);
+
+        Assertions.assertThat(modifiedString).isEqualTo(expectedString);
+    }
+
+    @Test
+    @Parameters({
+            "7, QixQix",
+            "14, Qix",
+            "21, FooQix",
+            "49, Qix"
+    })
+    public void should_return_Foo_when_the_number_is_a_multiple_of_7(int originalNumber, String expectedString) {
+        String modifiedString = FooBarQix.compute(originalNumber);
+
+        Assertions.assertThat(modifiedString).isEqualTo(expectedString);
+    }
+
+    @Test
+    @Parameters({
+            "3, FooFoo",
+            "13, Foo",
+            "23, Foo",
+            "33, FooFooFoo"
+    })
+    public void should_add_Foo_when_the_number_contains_a_3(int originalNumber, String expectedString) {
+        String modifiedString = FooBarQix.compute(originalNumber);
+
+        Assertions.assertThat(modifiedString).isEqualTo(expectedString);
+    }
+
+    @Test
+    @Parameters({
+            "5, BarBar",
+            "15, FooBarBar",
+            "25, BarBar",
+            "35, BarQixFooBar"
+    })
+    public void should_add_Foo_when_the_number_contains_a_5(int originalNumber, String expectedString) {
+        String modifiedString = FooBarQix.compute(originalNumber);
+
+        Assertions.assertThat(modifiedString).isEqualTo(expectedString);
+    }
+
+    @Test
+    @Parameters({
+            "7, QixQix",
+            "17, Qix",
+            "27, FooQix",
+            "37, FooQix"
+    })
+    public void should_add_Foo_when_the_number_contains_a_7(int originalNumber, String expectedString) {
+        String modifiedString = FooBarQix.compute(originalNumber);
+
+        Assertions.assertThat(modifiedString).isEqualTo(expectedString);
+    }
+
+    @Test
+    public void should_return_the_correct_string_when_given_a_number() {
+        int originalNumber = 35;
+        String expectedString = "BarQixFooBar";
+
+        String modifiedString = FooBarQix.compute(originalNumber);
+
+        Assertions.assertThat(modifiedString).isEqualTo(expectedString);
+    }
+}
